f52da6b2925eab05d2bf5dfb9b3bf895669275c8
arifamuhsy <arifamuhammads@gmail.com>
2022-03-07 12:44:33 +0700

    tambah function

--- hitungpajak.js
+++ hitungpajak.js
@@ -1,6 +1,7 @@
 const TidakKenaPajak = 50000000
-function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
-    // console.log(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount)
+
+function penghasilanKenaPajak(annualIncome)
+{
     var pkp = annualIncome - TidakKenaPajak
     if (pkp <= 200000000){
         return pkp * .1
@@ -13,5 +14,10 @@ function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChil
     if (pkp > 450000000){
         return pkp * .2
     }
+}
+
+function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
+    // console.log(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount)
+    return penghasilanKenaPajak(annualIncome)
 }
 console.log("Pajak",calculateTax(2024,'arifa',350000000,'menikah',2))
